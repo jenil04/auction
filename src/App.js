3796aeb65e4fca133f11dcc2cafc27b31cd5d566
Jenil Thakker <jenilkaushik.thakker@sjsu.edu>
2019-07-17 23:09:53 -0700

    Added bootstrap, particles..

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import React, { Component } from 'react';
-import web3 from './web3';
-import auction from './auction';
+import React, { Component } from "react";
+import web3 from "./web3";
+import auction from "./auction";
+import Particles from "react-particles-js";
+import { Layout } from "./Layout";
+import "./App.css";
+import { Button } from "react-bootstrap";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state = {bids: [], seller: "", auctionValue: "", highestBidder: "",
-                  value: "", message: "", name: "", bidAmount: ""};
+    this.state = {
+      i: 0,
+      maintxt: "Decentralized Auction on the Ethereum Blockchain",
+      speed: 100,
+      displaytxt: "",
+      tmpTitle: "Calculating number of active users",
+      fullTitle: "There are currently {`this.state.bids.length`} ",
+      j: 0,
+      bids: [],
+      seller: "",
+      auctionValue: "",
+      highestBidder: "",
+      value: "",
+      message: "",
+      name: "",
+      bidAmount: ""
+    };
   }
-  
+
   async componentDidMount() {
-    const seller = await auction.methods.seller().call(); 
+    this.timeout = setInterval(() => {
+      if (this.state.i < this.state.maintxt.length) {
+        let newI = this.state.i + 1;
+        this.setState({ i: newI });
+      }
+    }, 80);
+    this.timeout = setInterval(() => {
+      if (this.state.j < this.state.tmpTitle.length) {
+        let newJ = this.state.j + 1;
+        this.setState({ j: newJ });
+      }
+    }, 200);
+
+    const seller = await auction.methods.seller().call();
     const highestBidder = await auction.methods.getHighestBidder().call();
-    const auctionValue = await web3.eth.getBalance('0x4098878608e26825d248f7996267B49f2d1Bd49e'); 
+    const auctionValue = await web3.eth.getBalance(
+      "0x4098878608e26825d248f7996267B49f2d1Bd49e"
+    );
 
     this.setState({
       seller,
@@ -22,29 +56,29 @@ class App extends Component {
     });
   }
 
-  onEnter = async (event) => { 
-    event.preventDefault(); 
+  onEnter = async event => {
+    event.preventDefault();
     const accounts = await web3.eth.getAccounts();
 
     this.setState({
-      message: 'Processing...'
+      message: "Processing..."
     });
 
-    await auction.methods.submitBid().send({ 
-      from: accounts[0], 
-      valuecd: web3.utils.toWei(this.state.value, 'ether')
+    await auction.methods.submitBid().send({
+      from: accounts[0],
+      valuecd: web3.utils.toWei(this.state.value, "ether")
     });
 
     this.setState({
-      message: 'You have been entered into the auction!'
+      message: "You have been entered into the auction!"
     });
-  }
+  };
 
   onClick = async () => {
     const accounts = await web3.eth.getAccounts();
 
     this.setState({
-      message: 'Picking the highest bidder...'
+      message: "Picking the highest bidder..."
     });
 
     await auction.methods.determineWinner().send({
@@ -52,39 +86,70 @@ class App extends Component {
     });
 
     this.setState({
-      message: 'Sold, to the highest bidder!'
+      message: "Sold, to the highest bidder!"
     });
-  }
+  };
 
   render() {
+    let displaytext = this.state.maintxt.substring(0, this.state.i);
+    let displayTitle = "";
+    if (this.state.j >= this.state.tmpTitle.length) {
+      displayTitle = this.state.fullTitle;
+    } else {
+      displayTitle = this.state.tmpTitle.substring(0, this.state.j);
+    }
     return (
-      <div>
-        <h2>Decentralized Auction</h2>
-        <p>This contract is managed by {this.state.seller}</p>
-        <p>There are currently {this.state.bids.length} people entered, 
-        competing to buy {web3.utils.fromWei(this.state.auctionValue, 'ether')} ether! </p>
-        <hr />
-
-        <form onSubmit={this.onEnter}>
-          <h4>Join in to the Auction!</h4>
-          <div>
-            <label>Amount of ether to enter</label>
-            <input 
-              value={this.state.value}
-              onChange={event => this.setState({ value: event.target.value })}
-            />
+      <React.Fragment>
+        <Layout>
+          <Particles
+            className="bg-cover-image fixed w-screen h-screen z-n1"
+            params={{
+              particles: {
+                number: {
+                  value: 50
+                },
+                size: {
+                  value: 3
+                }
+              },
+              interactivity: {
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "repulse"
+                  }
+                }
+              }
+            }} 
+          >
+            {" "}
+          </Particles>
+          <div class="justify-center">
+              <h1 class="text-white justify-center text-center" >{displaytext}</h1>
+                <h5 class="w-full block text-white text-5xl font-fancy font-bold text-center justify-center mb-8">
+                  There are currently {this.state.bids.length} people entered,
+                  competing to buy{" "}
+                  {web3.utils.fromWei(this.state.auctionValue, "ether")} ether{" "}
+                </h5>
+                <form onSubmit={this.onEnter} >
+                  <h4 class="text-white justify-center" >Join the Auction</h4>
+                  <div>
+                    <label class="text-white justify-center font-bold text-center">Bid amount: </label>
+                    <input
+                      value={this.state.value}
+                      onChange={event =>
+                        this.setState({ value: event.target.value })
+                      }
+                    />
+                  </div>
+                  <Button class= "text-center">Make a bid</Button>
+                </form>
+                <h4 class="text-white justify-center" >Reaveal the Highest Bidder</h4>
+                <Button onClick={this.onClick} class = "text-center">Highest Bidder</Button>
+                <h4>{this.state.message}</h4>
           </div>
-          <button>Enter</button>
-        </form> 
-        
-        <hr />
-        <h4>Let's find out the highest bidder</h4>
-        <button onClick={this.onClick}>Highest Bidder</button>
-
-        <hr />
-
-        <h1>{this.state.message}</h1>
-      </div>
+        </Layout>
+      </React.Fragment>
     );
   }
 }
--- src/auction.js
+++ src/auction.js
@@ -1,87 +1,87 @@
-import web3 from './web3';
+import web3 from "./web3";
 
 let auctionABI = [
   {
-    "constant": true,
-    "inputs": [],
-    "name": "seller",
-    "outputs": [
+    constant: true,
+    inputs: [],
+    name: "seller",
+    outputs: [
       {
-        "name": "",
-        "type": "address"
+        name: "",
+        type: "address"
       }
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function",
-    "signature": "0x08551a53"
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+    signature: "0x08551a53"
   },
   {
-    "constant": true,
-    "inputs": [
+    constant: true,
+    inputs: [
       {
-        "name": "",
-        "type": "uint256"
+        name: "",
+        type: "uint256"
       }
     ],
-    "name": "bidders",
-    "outputs": [
+    name: "bidders",
+    outputs: [
       {
-        "name": "",
-        "type": "address"
+        name: "",
+        type: "address"
       }
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function",
-    "signature": "0xcff29dfd"
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+    signature: "0xcff29dfd"
   },
   {
-    "constant": false,
-    "inputs": [],
-    "name": "auction",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function",
-    "signature": "0x7d9f6db5"
+    constant: false,
+    inputs: [],
+    name: "auction",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+    signature: "0x7d9f6db5"
   },
   {
-    "constant": false,
-    "inputs": [],
-    "name": "submitBid",
-    "outputs": [],
-    "payable": true,
-    "stateMutability": "payable",
-    "type": "function",
-    "signature": "0xc334b6b2"
+    constant: false,
+    inputs: [],
+    name: "submitBid",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+    signature: "0xc334b6b2"
   },
   {
-    "constant": false,
-    "inputs": [],
-    "name": "determineWinner",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function",
-    "signature": "0x33b16d93"
+    constant: false,
+    inputs: [],
+    name: "determineWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+    signature: "0x33b16d93"
   },
   {
-    "constant": true,
-    "inputs": [],
-    "name": "getHighestBidder",
-    "outputs": [
+    constant: true,
+    inputs: [],
+    name: "getHighestBidder",
+    outputs: [
       {
-        "name": "",
-        "type": "address"
+        name: "",
+        type: "address"
       }
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function",
-    "signature": "0x003495a4"
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+    signature: "0x003495a4"
   }
-]
-let auctionAddress ='0x4098878608e26825d248f7996267B49f2d1Bd49e';
+];
+let auctionAddress = "0x4098878608e26825d248f7996267B49f2d1Bd49e";
 
 export default new web3.eth.Contract(auctionABI, auctionAddress);
